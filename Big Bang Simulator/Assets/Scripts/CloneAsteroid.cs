eeef6a9cdaf676e20914d07e87674082c194c2dc
ptltorres <pedroltg78@hotmail.com>
2017-01-03 15:07:06 -0500

    Phase 3

---
+++ Big Bang Simulator/Assets/Scripts/CloneAsteroid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneAsteroid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            var randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            Instantiate(Resources.Load("Asteroid"), new Vector3(Random.Range(-40, 40), Random.Range(-40, 40), Random.Range(-40, 40)), randomRotation);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
