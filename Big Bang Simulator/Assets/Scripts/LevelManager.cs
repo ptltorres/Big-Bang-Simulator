45bc411be2127371bd3ca65332f5b4edd428b2b9
Clara Fairbanks <clarafairbanks@gmail.com>
2017-01-12 12:18:11 -0500

    updated scenes and trans
    Updated scenes, reorganized files and folders, deleted unnecessary
    scenes, script updates, added scene builder

---
+++ Big Bang Simulator/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	float time = 7;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time > 0) {
+			time -= Time.deltaTime;
+		} else {
+			//load next level
+			loadNextLevel();
+		}
+	}
+	public void loadNextLevel(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
---
+++ Big Bang Simulator/Assets/Scripts/StartBigBang.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class StartBigBang : MonoBehaviour {
+
+
+	public ParticleSystem BigBangInitial;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void toggleOnOff(){
+		//BigBangInitial.gameObject.SetActive(BigBangInitial.gameObject.activeSelf);
+		if (BigBangInitial.isPlaying) {
+			BigBangInitial.Stop ();
+		} else {
+			BigBangInitial.Play ();
+		}
+	}
+
+}
