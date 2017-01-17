f176faf56bc5560f852747c5e1cc684d2b887ae0
ptltorres <pedroltg78@hotmail.com>
2017-01-17 14:45:55 -0500

    Cursor and Gaze Gestures Added

---
+++ Big Bang Simulator/Assets/Scripts/Exploder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploder : MonoBehaviour {
+    public GameObject bigBang;
+    private static bool exploded = false;
+
+    public void OnSelect() {
+        if (!exploded) {
+            Destroy(gameObject);
+            exploded = true;
+            Instantiate(bigBang, new Vector3(0f,0.5f,100f), Quaternion.identity);
+        }
+    }
+}
+
